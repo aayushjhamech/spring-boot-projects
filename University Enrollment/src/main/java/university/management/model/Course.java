c81b81ee9356f281f3637ecb3da780b31f984d0d
Kumar <aayushkumar7@deloitte.com>
2022-07-12 10:45:09 +0530

    University First Commit

---
+++ University Enrollment/src/main/java/university/management/model/Course.java
@@ -0,0 +1,41 @@
+package university.management.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long courseId;
+
+    private String title;
+    private String description;
+    private int numberOfCredits;
+
+    public Course() {
+    }
+
+    public Course(String title, String description, int numberOfCredits) {
+        this.title = title;
+        this.description = description;
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ University Enrollment/src/main/java/university/management/model/Student.java
@@ -0,0 +1,82 @@
+package university.management.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, length = 60)
+    private String firstName;
+    
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private LocalDate dateOfBirth;
+
+    public Student() {
+    }
+
+    public Student(String firstName,
+                   String lastName,
+                   String email,
+                   LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+}
